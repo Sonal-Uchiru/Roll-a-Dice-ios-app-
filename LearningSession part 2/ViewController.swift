213a3f1bdb786af1e351f070b4b5caa6af097352
sonal-uchiru <sonal123326@gmail.com>
2021-12-16 18:13:28 +0530

    Initial Commit

---
+++ LearningSession part 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearningSession part 2
+//
+//  Created by sonal jayawardana on 2021-12-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
